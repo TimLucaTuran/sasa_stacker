trying to implement a running average layer via fixed weight convolutions

sasa_stacker/experiment.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Lambda, Activation, MaxPooling1D, Concatenate, Dropout, Conv1D, GlobalMaxPooling1D, Reshape, BatchNormalization, Flatten
+from tensorflow.keras.layers import *
 from tensorflow.keras.losses import mean_squared_error, Huber
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model, load_model
@@ -16,8 +16,33 @@ from hyperparameters import *
 from sasa_db.crawler import Crawler
 from testing import show_stack_info
 import sqlite3
-#%%
 
+class RunningAvg(tf.keras.layers.Layer):
+    def __init__(self, N):
+        self.N = N
+        super(RunningAvg, self).__init__()
+
+    def _running_avg_2d(self, x):
+        #print("[INFO] numpy input:", x.shape)
+        batch_dim = x.shape[0]
+        z_dim = x.shape[2]
+        for i in range(batch_dim):
+            for j in range(z_dim):
+                x[i,:,j] = np.convolve(x[i,:,j], np.ones((self.N,))/self.N, mode='same')
+        return x
+
+    def call(self, input):
+        #print("[INFO] Avg Input:", input)
+        input_shape = input.get_shape()
+
+        out_tensor = tf.numpy_function(
+            func = self._running_avg_2d,
+            inp = [input],
+            Tout = float,
+        )
+        out_tensor.set_shape(input_shape)
+        return out_tensor
+#%%
 gen = batch_generator("data/bili_validation")
 continuous_out_loss = tf.Variable(1/40000)
 #conn = sqlite3.connect("data/NN_smats.db")
@@ -74,6 +99,68 @@ class SasaLayer(tf.keras.layers.Layer):
     def call(self, inputs):
         return call_op(inputs)
 
+
+#%%
+with CustomObjectScope({'loss': mse_with_changable_weight(continuous_out_loss)}):
+            old_model = load_model("data/models/more_kernels__forward.h5")
+
+w = np.asarray([
+    [
+    [
+    [0,0],
+    [0,0],
+    [0,0]
+    ],
+    [
+    [0,0],
+    [0,0],
+    [0,0]
+    ]
+    ]
+    ])
+old_model.layers
+l = old_model.layers[9].get_weights()[0]
+l.shape
+RunningAvg = Conv1D(
+    filters = 2,
+    kernel_size = 3,
+    padding='same',
+    name="RunningAvg",
+    )
+RunningAvg.trainable = False
+x = RunningAvg(old_model.output)
+model = Model(inputs=old_model.input, outputs=x)
+
+l = RunningAvg.get_weights()
+l[0].shape
+
+RunningAvg.set_weights(w)
+
+opt = Adam()
+model.compile(optimizer=opt, loss="mse", metrics=['accuracy'])
+
+spec, design = gen.__next__()
+design[0] = design[0].astype(float)
+spec_ = model(design)
+wav = np.linspace(0.4, 1.2, 160)
+
+for n in range(20, 30):
+    plt.plot(wav, spec[n,:,1])
+    plt.plot(wav, spec_[n,:,1])
+    plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(wav, x[22,:,1])
+ax.plot(wav, x_[22,:,1])
+fig.savefig("data/plots/combined2.pdf")
+
+model.fit(x, x)
+model.summary()
+for layer in model.layers:
+    print(layer.name)
+layer = model.get_layer(name="conv1d")
+layer.kernel.name
+
 #%%
 with CustomObjectScope({'loss': mean_squared_error}):
     model = load_model("data/models/corrected3_forward.h5")
@@ -94,72 +181,3 @@ stack = "data/square_validation/X/2020-03-10_11:52:38.181391.npy"
 index = 0
 batch_dir = "data/square_validation"
 show_stack_info(model, stack, index, batch_dir)
-#%%
-with CustomObjectScope({'loss': mse_with_changable_weight(continuous_out_loss)}):
-            old_model = load_model("data/models/bili.h5")
-
-
-
-x = SasaLayer()(old_model.output)
-x = Activation('linear')(x)
-model = Model(inputs=old_model.input, outputs=x)
-
-
-opt = Adam()
-model.compile(optimizer=opt, loss="mse", metrics=['accuracy'])
-x, y = gen.__next__()
-x.shape
-x_ = model(x)
-x_.shape
-wav = np.linspace(0.4, 1.2, 160)
-
-for n in range(20, 30):
-    plt.plot(wav, x[n,:,1])
-    plt.plot(wav, x_[n,:,1])
-    plt.show()
-
-fig, ax = plt.subplots()
-ax.plot(wav, x[22,:,1])
-ax.plot(wav, x_[22,:,1])
-fig.savefig("data/plots/combined2.pdf")
-
-model.fit(x, x)
-model.summary()
-for layer in model.layers:
-    print(layer.name)
-layer = model.get_layer(name="conv1d")
-layer.kernel.name
-#%%
-
-inp = Input(shape=(MODEL_INPUTS, 2))
-x = Conv1D(64, 5, activation='relu')(inp)
-x = MaxPooling1D()(x)
-x = Conv1D(64, 5, activation='relu')(x)
-x = MaxPooling1D()(x)
-x = Conv1D(128, 5, activation='relu')(x)
-x = MaxPooling1D()(x)
-x = Conv1D(256, 5, activation='relu')(x)
-conv_out = GlobalMaxPooling1D()(x)
-
-#discrete branch
-x = Dense(256, activation='relu')(conv_out)
-x = Dropout(0.5)(x)
-discrete_out = Dense(MODEL_DISCRETE_OUTPUTS, activation='sigmoid', name='discrete_out')(x)
-
-#continuous branch
-x = Dense(256, activation='relu')(conv_out)
-x = BatchNormalization()(x)
-continuous_out = Dense(MODEL_CONTINUOUS_OUTPUTS, activation='linear', name='continuous_out')(x)
-
-x = SasaLayer()([discrete_out, continuous_out])
-model = Model(inputs=inp, outputs=x)
-
-x = Lambda(call)(old_model.output)
-
-def call(inputs):
-    batch_size = inputs[0].get_shape().dims[0].value
-    print("[INFO] SasaLayer called batch size", batch_size)
-    out_tensor = tf.numpy_function(func=my_func, inp=inputs, Tout=tf.float32)
-    out_tensor.set_shape(tf.TensorShape([batch_size,160,2]))
-
-    return out_tensor

sasa_stacker/train.py
@@ -27,7 +27,33 @@ from hyperparameters import *
 
 INIT_LR = 1e-3
 #%%
-def moving_average(x):
+
+
+class RunningAvg(tf.keras.layers.Layer):
+    def __init__(self, N):
+        self.N = N
+        super(RunningAvg, self).__init__()
+
+    def _running_avg_2d(self, x):
+        #print("[INFO] numpy input:", x.shape)
+        batch_dim = x.shape[0]
+        z_dim = x.shape[2]
+        for i in range(batch_dim):
+            for j in range(z_dim):
+                x[i,:,j] = np.convolve(x[i,:,j], np.ones((self.N,))/self.N, mode='same')
+        return x
+
+    def call(self, input):
+        #print("[INFO] Avg Input:", input)
+        input_shape = input.get_shape()
+
+        out_tensor = tf.numpy_function(
+            func = self._running_avg_2d,
+            inp = [input],
+            Tout = float,
+        )
+        out_tensor.set_shape(input_shape)
+        return out_tensor
 
 def create_model():
     inp = Input(shape=(MODEL_INPUTS, 2))
@@ -65,13 +91,15 @@ def create_forward_model():
     x = UpSampling1D()(x) #40,64
 
     x = Conv1D(64, 3, activation='relu', padding='same')(x)
-    x = Lambda(lambda x: )()
+    x = RunningAvg(3)(x)
     x = UpSampling1D()(x) #80,64
 
     x = Conv1D(32, 3, activation='relu', padding='same')(x)
+    x = RunningAvg(3)(x)
     x = UpSampling1D()(x) #160,128
 
     x = Conv1D(2, 3, activation='linear', padding='same')(x) #160,2
+    x = RunningAvg(3)(x)
     x = BatchNormalization()(x)
     model = Model(inputs=[dis_in, cont_in], outputs=x)
     return model